8a0a45c2ee49a62abd4fd473992239b3c8f1541f
MicroDiamond5004 <serepetr90@mail.ru>
2025-03-26 13:55:21 -0400

    Добавлено представление фильтров с правильными названиями

--- src/components/apps/tickets/TicketFilter.tsx
+++ src/components/apps/tickets/TicketFilter.tsx
@@ -1,13 +1,12 @@
-import { Box, Button, Grid2 as Grid, Typography, styled } from '@mui/material';
-import { Stack } from '@mui/system';
-import { IconList, IconListDetails } from '@tabler/icons-react';
-import React, { useContext } from "react";
+import { Box, Grid2 as Grid, Typography, styled } from '@mui/material';
+import { useContext } from "react";
 import { TicketContext } from "src/context/TicketContext";
 
 const BoxStyled = styled(Box)(() => ({
-  padding: '30px',
+  padding: '30px 22px',
   transition: '0.1s ease-in',
   cursor: 'pointer',
+  height: '127px',
   color: 'inherit',
   '&:hover': {
     transform: 'scale(1.03)',
@@ -17,22 +16,23 @@ const BoxStyled = styled(Box)(() => ({
 const TicketFilter = () => {
 
   const { tickets, setFilter }: any = useContext(TicketContext);
-  const pendingC = tickets.filter((t: { Status: string; }) => t.Status === 'Pending').length;
-  const openC = tickets.filter((t: { Status: string; }) => t.Status === 'Open').length;
-  const closeC = tickets.filter((t: { Status: string; }) => t.Status === 'Closed').length;
-
-
+  const openC = tickets.filter((t: { Status: string; }) => t.Status === 'Новый заказ').length;
+  const pendingC = tickets.filter((t: { Status: string; }) => t.Status === 'Принято в работу').length;
+  const formedC = tickets.filter((t: { Status: string; }) => t.Status === 'Забронировано').length;
+  const bookedC = tickets.filter((t: { Status: string; }) => t.Status === 'Оформлено').length;
+  const closeC = tickets.filter((t: { Status: string; }) => t.Status === 'Завершено').length;
+  
 
   return (
     (
       <Grid container spacing={3} textAlign="center">
       <Grid
         size={{
-          lg: 3,
+          lg: 2,
           sm: 6,
           xs: 12
         }}>
-        <BoxStyled
+        <BoxStyled alignContent='center'
           onClick={() => setFilter('total_tickets')}
           sx={{ backgroundColor: 'primary.light', color: 'primary.main' }}
         >
@@ -42,44 +42,72 @@ const TicketFilter = () => {
       </Grid>
       <Grid
         size={{
-          lg: 3,
+          lg: 2,
           sm: 6,
           xs: 12
         }}>
-        <BoxStyled
+        <BoxStyled alignContent='center'
           onClick={() => setFilter('Pending')}
           sx={{ backgroundColor: 'warning.light', color: 'warning.main' }}
         >
           <Typography variant="h3">{pendingC}</Typography>
-          <Typography variant="h6">В Ожидании</Typography>
+          <Typography variant="h6">Новый заказ</Typography>
         </BoxStyled>
       </Grid>
       <Grid
         size={{
-          lg: 3,
+          lg: 2,
           sm: 6,
           xs: 12
         }}>
-        <BoxStyled
+        <BoxStyled alignContent='center'
           onClick={() => setFilter('Open')}
           sx={{ backgroundColor: 'success.light', color: 'success.main' }}
         >
           <Typography variant="h3">{openC}</Typography>
-          <Typography variant="h6">Действующие</Typography>
+          <Typography variant="h6">Принято в работу</Typography>
         </BoxStyled>
       </Grid>
       <Grid
         size={{
-          lg: 3,
+          lg: 2,
           sm: 6,
           xs: 12
         }}>
-        <BoxStyled
+        <BoxStyled alignContent='center'
+          onClick={() => setFilter('Closed')}
+          sx={{ backgroundColor: 'pink', color: 'darkpink' }}
+        >
+          <Typography variant="h3">{closeC}</Typography>
+          <Typography variant="h6">Забронировано</Typography>
+        </BoxStyled>
+      </Grid>
+      <Grid
+        size={{
+          lg: 2,
+          sm: 6,
+          xs: 12
+        }}>
+        <BoxStyled alignContent='center'
+          onClick={() => setFilter('Closed')}
+          sx={{ backgroundColor: '#a52a2a1f', color: 'brown' }}
+        >
+          <Typography variant="h3">{closeC}</Typography>
+          <Typography variant="h6">Оформлено</Typography>
+        </BoxStyled>
+      </Grid>
+      <Grid
+        size={{
+          lg: 2,
+          sm: 6,
+          xs: 12
+        }}>
+        <BoxStyled alignContent='center'
           onClick={() => setFilter('Closed')}
           sx={{ backgroundColor: 'error.light', color: 'error.main' }}
         >
           <Typography variant="h3">{closeC}</Typography>
-          <Typography variant="h6">Завершенные</Typography>
+          <Typography variant="h6">Завершено</Typography>
         </BoxStyled>
       </Grid>
     </Grid>)
